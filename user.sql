2d31178092c7b89726772696fc2676826f423a90
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-06-21 10:01:01 +0700

    added sql

---
+++ user.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE IF NOT EXISTS `User` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `is_active` smallint(6) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `User`
+--
+
+INSERT INTO `User` (`id`, `email`, `password`, `is_active`) VALUES
+(1, 'admin@admin.com', '$2y$10$bTNzM0NyM3RTNGx0eTM0a.AOX409D3S8mIGSNBCUKpaAwF9zrxMZu', 1);
